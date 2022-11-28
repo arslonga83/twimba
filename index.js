24a5d38223c698f46e426758cc08d401454fc871
arslonga83 <jlwood83@gmail.com>
2022-11-27 21:17:50 -0800

    build retweet function

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ import { tweetsData } from "./data.js";
  document.addEventListener('click', (e) => {
     if (e.target.dataset.like) {
         handleLikeClick(e.target.dataset.like)
+    } else if (e.target.dataset.retweet) {
+        handleRetweetClick(e.target.dataset.retweet)
     }
  })
 
@@ -23,9 +25,20 @@ import { tweetsData } from "./data.js";
             targetTweetObj.likes--; 
         }
         targetTweetObj.isLiked = !targetTweetObj.isLiked;
-        render();
-        
-    
+        render();  
+ }
+
+ function handleRetweetClick(tweetId) {
+    const targetTweetObj = tweetsData.filter((tweet) => {
+        return tweet.uuid === tweetId;
+    })[0]
+    if (!targetTweetObj.isRetweeted) {
+        targetTweetObj.retweets++
+    } else {
+        targetTweetObj.retweets--
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+    render();
  }
 
 function getFeedHtml(){
